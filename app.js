a35630f9177f7c5f146dba108d1654c779363d50
Steven Le <steven.an.le@gmail.com>
2018-12-27 20:40:47 -0800

    Create routes for tweets

--- app.js
+++ app.js
@@ -1,15 +1,37 @@
 const mongoose = require('mongoose');
-
+const users = require("./routes/api/users");
+const tweets = require("./routes/api/tweets");
 const express = require("express");
 const app = express();
 const db = require("./config/keys").mongoURI;
+const User = require('./models/User');
+const bodyParser = require("body-parser");
+const passport = require("passport");
+
 
 mongoose
   .connect(db, { useNewUrlParser: true })
   .then(() => console.log("Connected to MongoDB successfully"))
   .catch(err => console.log(err));
 
-app.get("/", (req, res) => res.send("Hello world"));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use(passport.initialize());
+require("./config/passport")(passport);
+
+app.get("/", (req, res) => {
+  const user = new User ({
+    handle: 'jim',
+    email: 'jim@jim.jim',
+    password: 'jimisgreat123'
+  })
+  user.save()
+  res.send("hello world!");
+})
+
+app.use("/api/users", users);
+app.use("/api/tweets", tweets);
 
 const port = process.env.PORT || 5000;
 
